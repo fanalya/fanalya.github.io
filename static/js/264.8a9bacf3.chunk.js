1eb824d66e4779d37d3ff3fe5ed86ed7c0b52077
O\ydung <ydungkuan@outlook.com>
2024-04-01 22:54:16 +0700

    Update

--- static/js/264.8a9bacf3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcrm_frontend=self.webpackChunkcrm_frontend||[]).push([[264],{264:(e,s,a)=>{a.r(s),a.d(s,{default:()=>r});var n=a(579);const r=()=>(document.title="Dashboard",(0,n.jsx)(n.Fragment,{children:"asasdasd"}))}}]);
-//# sourceMappingURL=264.8a9bacf3.chunk.js.map
